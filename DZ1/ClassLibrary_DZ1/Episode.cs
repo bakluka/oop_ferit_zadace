d6fd54ab29e9e92e2e68bb4c9354fd958a4f01b2
bakluka <bakluka@gmail.com>
2020-10-27 17:44:15 +0100

    Add AV1 task files

---
+++ DZ1/ClassLibrary_DZ1/Episode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassLibrary_DZ1
+{
+    public class Episode
+    {
+        private int viewers;
+        private double total_score;
+        private double max_score;
+
+        public Episode()
+        {
+            viewers = 0;
+            total_score = 0;
+            max_score = 0;
+        }
+        public Episode(int viewer_input, double total_score_input, double max_score_input)
+        {
+            viewers = viewer_input;
+            total_score = total_score_input;
+            max_score = max_score_input;
+        }
+
+        public void AddView(double score_to_add)
+        {
+            viewers++;
+            total_score += score_to_add; 
+            if (score_to_add > max_score)
+            {
+                max_score = score_to_add;
+            }
+        }
+        public double GetMaxScore()
+        {
+            return max_score;
+        }
+
+        public double GetAverageScore()
+        {
+            double average = total_score / viewers;
+            return average;
+        }
+
+        public int GetViewerCount()
+        {
+            return viewers;
+        }
+    }
+}
---
+++ DZ1/DZ1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ClassLibrary_DZ1;
+
+namespace DZ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Episode ep1, ep2;
+            ep1 = new Episode();
+            ep2 = new Episode(10, 64.39, 8.7);
+            int viewers = 10;
+            for (int i = 0; i < viewers; i++)
+            {
+                ep1.AddView(GenerateRandomScore());
+                Console.WriteLine(ep1.GetMaxScore());
+            }
+            if (ep1.GetAverageScore() > ep2.GetAverageScore())
+            {
+                Console.WriteLine($"Viewers: {ep1.GetViewerCount()}");
+            }
+            else
+            {
+                Console.WriteLine($"Viewers: {ep2.GetViewerCount()}");
+            }
+
+        }
+        public static double GenerateRandomScore()
+        {
+            Random random_number = new Random();
+            int result = random_number.Next(10, 100);  //vraca slicne brojeve, seed se neznacajno mjenja kroz for
+            return (Double)result / 10.00;
+        }
+    }
+}
